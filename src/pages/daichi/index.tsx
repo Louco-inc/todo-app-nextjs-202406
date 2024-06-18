6fd2ffbc76db7b67c138492fe314c03be1a3434e
KuRa04 <mwamkuracchi@gmail.com>
2024-06-18 20:08:43 +0900

    feat: header作成

--- src/pages/daichi/index.tsx
+++ src/pages/daichi/index.tsx
@@ -1,11 +1,27 @@
 export default function IndexComponent() {
   return (
-    <div className="border border-gray-400 rounded-2xl p-2 m-2 flex justify-around items-center">
-      <h1 className="text-3xl font-bold underline">
-        Hello Tailwind CSS!
-      </h1>
+    <div className="">
+      <header className="h-16 bg-blue p-4  flex justify-between content-center">
+        <h1 className="text-3xl text-white font-bold ">Todo App</h1>
+        <div>
+          <button className="w-fit h-8 text-white text-base bg-transparent border border-gray rounded-lg px-4 mx-2">
+            kaichi
+          </button>
+          <button className="w-fit h-8 text-white text-base bg-transparent border border-gray rounded-lg px-4 mx-2">
+            daichi
+          </button>
+          <button className="w-fit h-8 text-white text-base bg-transparent border border-gray rounded-lg px-4 mx-2">
+            morishun
+          </button>
+          <button className="w-fit h-8 text-white text-base bg-transparent border border-gray rounded-lg px-4 mx-2">
+            naoya
+          </button>
+        </div>
+      </header>
       <p className="m-0 text-gray-400">Tailwind CSSです</p>
-      <button className="bg-gray-300 border-0 p-2 rounded-md hover:bg-gray-400 hover:text-white">ボタン</button>
+      <button className="bg-gray-300 border-0 p-2 rounded-md hover:bg-gray-400 hover:text-white">
+        ボタン
+      </button>
     </div>
   );
 }
